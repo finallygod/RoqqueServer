9bfdcd04e45c4030165c98a801055a28679a5b2e
finallygod <finallygoddidit@gmail.com>
2025-07-02 12:00:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "finallygoddidit@gmail.com";
-//const pass = "rargxrpcvrasjzyn";
-// tevez 17 JUNE
+const pass = "rargxrpcvrasjzyn";
+// tevez 2 AUGUST
 
 // Middleware
 app.use(express.json());
